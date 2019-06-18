ea8f83e609a3edfb9362098978febcd5f24b1c7f
Tanner Nielsen <tannerntannern@gmail.com>
2019-06-18 17:54:37 -0500

    General cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import Network from './resources';
-
-let wages = Network.supply('Wages', 2000);
-let rent = Network.consumer('Rent');
-let other = Network.consumer('Other');
-
-rent.consumes(1322).from(wages);
-other.consumesAsMuchAsPossible().from(wages);
-
-let results = Network.solve();
-
-console.log('\n=== Transfer Summary ===');
-results.transfers.forEach((from, to, value) => {
-    if (value.value() > 0)
-        console.log(`${from.name} --> ${value.value()}) --> ${to.name}`);
-});
-console.log();
+export * from './resources';
