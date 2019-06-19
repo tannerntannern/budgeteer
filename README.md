c25b7672927cd5a84f8838729950c0ca1f875ed1
Tanner Nielsen <tannerntannern@gmail.com>
2019-06-18 19:39:10 -0500

    Began some docs

---
+++ README.md
@@ -0,0 +1,23 @@
+# 🎩 budgeteer
+> a specialized constraint solver for budget flows
+
+<!-- TODO: insert link to website -->
+
+# Overview
+Budgeteer allows you to effortlessly balance a budget without doing any monotonous work.  It allows you to intuitively define resource flows and budgeteer will balance it for you.  "Resource" is purposely non-specific here -- you can use this tool to balance a monetary budget, manage your time, track calories, or whatever you want.
+
+Budgeteer is written in TypeScript and relies on [kiwi.js](https://github.com/IjzerenHein/kiwi.js/) to do constraint solving under the hood.
+
+## Disclaimer
+Budgeteer is a hobby-tool I built for myself.  I would not advise using it in production.
+
+# Installation
+Coming soon...
+
+# Usage example
+Coming soon...
+
+# Author
+Tanner Nielsen <tannerntannern@gmail.com>
+
+[Website](https://tannernielsen.com) | [GitHub](https://github.com/tannerntannern)
--- src/two-key-map.ts
+++ src/two-key-map.ts
@@ -1,9 +1,15 @@
 /**
- * A Map with two keys.
+ * A Map with two keys as opposed to one.  Order does matter, so `map.get(k1, k2) !== map.get(k2, k1)` for example.
  */
 export class TwoKeyMap<K, V> {
+    /**
+     * Internal storage for the TwoKeyMap, which is just a Map of Maps.
+     */
     private values: Map<K, Map<K, V>> = new Map();
 
+    /**
+     * Creates a relationship between `[k1, k2]` and `v`, overwriting if one already exists.
+     */
     public set (k1: K, k2: K, val: V) {
         if (!this.values.get(k1))
             this.values.set(k1, new Map());
@@ -11,6 +17,9 @@ export class TwoKeyMap<K, V> {
         this.values.get(k1).set(k2, val);
     }
 
+    /**
+     * Gets the value associated with `[k1, k2]`.
+     */
     public get (k1: K, k2: K) {
         if (!this.values.get(k1))
             return null;
@@ -18,6 +27,9 @@ export class TwoKeyMap<K, V> {
             return this.values.get(k1).get(k2);
     }
 
+    /**
+     * Similar to JavaScript's `Map.prototype.forEach`, but with slightly different arguments.
+     */
     public forEach (handler: (k1: K, k2: K, val: V) => void) {
         this.values.forEach((internalMap, externalKey) => {
             internalMap.forEach((value, internalKey) => {
